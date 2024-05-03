f3e11bb4dd228a173e694674ec541c5717ba24c0
Nikolaj Bech Andersen <nikolaj.bech@live.dk>
2024-05-03 11:45:36 +0200

    Update db

--- src/server/db/index.ts
+++ src/server/db/index.ts
@@ -1,7 +1,23 @@
-import { drizzle } from 'drizzle-orm/postgres-js'
+import { type PostgresJsDatabase, drizzle } from 'drizzle-orm/postgres-js'
 import postgres from 'postgres'
 
 import { env } from '@/env.js'
 import * as schema from './schema'
 
-export const db = drizzle(postgres(env.DATABASE_URL), { schema })
+export type DB = PostgresJsDatabase<typeof schema>
+
+const globalForDrizzle = globalThis as unknown as {
+  db: DB
+}
+
+export const db =
+  globalForDrizzle.db ||
+  drizzle(
+    postgres(env.DATABASE_URL, {
+      max: process.env.NODE_ENV === 'development' ? 50 : undefined,
+      ssl: process.env.NODE_ENV !== 'development',
+    }),
+    { schema },
+  )
+
+if (env.NODE_ENV !== 'production') globalForDrizzle.db = db
