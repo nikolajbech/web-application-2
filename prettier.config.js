1a3a133495eb731a4c603085261bdfbac19c7cb9
Nikolaj Bech Andersen <nikolaj.bech@live.dk>
2024-05-26 18:38:45 +0200

    Add prettier-plugin-sort-imports

--- prettier.config.js
+++ prettier.config.js
@@ -1,10 +1,16 @@
-/** @type {import('prettier').Config & import('prettier-plugin-tailwindcss').PluginOptions} */
+/** @type {import('prettier').Config & import('prettier-plugin-tailwindcss').PluginOptions & import("@ianvs/prettier-plugin-sort-imports").PluginConfig} */
 const config = {
-  plugins: ['prettier-plugin-tailwindcss'],
+  plugins: [
+    '@ianvs/prettier-plugin-sort-imports',
+    'prettier-plugin-tailwindcss',
+  ],
   semi: false,
   singleQuote: true,
   jsxSingleQuote: true,
   trailingComma: 'all',
+  importOrder: ['<THIRD_PARTY_MODULES>', '', '^@/', '^[.][.]/', '^[.]/'],
+  importOrderParserPlugins: ['typescript', 'jsx', 'decorators-legacy'],
+  importOrderTypeScriptVersion: '4.4.0',
 }
 
 export default config
